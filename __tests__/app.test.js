baf6ad9de75f1bff11fbe3c536ae066bcd2d99ac
Jessica Kincaid <4j.kincaid@gmail.com>
2021-10-30 12:38:31 -0500

    board functions without overflow

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,7 @@
 /**
  * @jest-environment jsdom
  */
+<<<<<<< HEAD
 const app = require('../scripts/app');
 
 test('use jsdom in this test file', () => {
@@ -22,4 +23,16 @@ describe('Validate Move', () => {
     expect(app.isValidPosition(64)).toBe(false);
   })
 
+=======
+
+test('use jsdom in this test file', () => {
+    const element = document.createElement('div');
+    expect(element).not.toBeNull();
+});
+
+const app = require("../scripts/app")
+test('should be valid', () => {
+    const element = document.createElement('div');
+    expect(element).not.toBe(1)
+>>>>>>> 12df19a (restore package-lock and package.json)
 })
