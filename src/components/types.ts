6250278b9b8decf4179299bc9622b49418cb639e
Lex <lexluka1031@gmail.com>
2023-12-19 15:00:29 -0600

    linted types

---
+++ src/components/types.ts
@@ -0,0 +1,24 @@
+import { BoundingBox } from "framer-motion";
+import { RefObject } from "react";
+
+export interface Item {
+  type: string;
+  legalMoves?: string[];
+  id: string;
+  constraint?: Partial<BoundingBox> | RefObject<Element> | false;
+  onDragStartProp: (type: string) => void;
+  onDragEndProp: (type: string) => void;
+  onDragOverProp: (type: string) => void;
+}
+
+export interface BoardItem {
+  type: string;
+  id: string;
+}
+
+interface BoardRow {
+  id: string;
+  items: BoardItem[];
+}
+
+export type Board = BoardRow[];
