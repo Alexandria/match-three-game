632ce1dd5bf6a170c6d010db1c526fa8ebdea183
Lex <lexluka1031@gmail.com>
2023-12-19 16:51:00 -0600

    clean up index and app styles

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,14 +1,11 @@
-import React from 'react';
-import logo from '../../logo.svg';
-import './App.css';
-import { Board } from '../Board/Board';
+import React from "react";
+import "./App.css";
+import { Board } from "../Board/Board";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <Board/>
-      </header>
+      <Board />
     </div>
   );
 }
