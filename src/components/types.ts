32af789a76f0dd7b4b91aec3301b79784e4b2432
Lex <lexluka1031@gmail.com>
2024-01-15 11:52:45 -0600

    Cleaned up item types

--- src/components/types.ts
+++ src/components/types.ts
@@ -1,14 +1,8 @@
-import { BoundingBox } from "framer-motion";
-import { RefObject } from "react";
-
 export interface Item {
-  type: string;
-  legalMoves?: string[];
-  id: string;
-  constraint?: Partial<BoundingBox> | RefObject<Element> | false;
-  onDragStartProp: (type: string) => void;
-  onDragEndProp: (type: string) => void;
-  onDragOverProp: (type: string) => void;
+  item: BoardItem;
+  onDragStart: (type: string) => void;
+  onDragEnd: (type: string) => void;
+  onDragOver: (type: string) => void;
 }
 
 export interface BoardItem {
