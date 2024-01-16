5b775b993a6cbd79a640b4205a94aa76126aefdd
Lex <lexluka1031@gmail.com>
2024-01-15 18:59:15 -0600

    Update app test

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './index';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./index";
 
-test('renders learn react link', () => {
+test("renders learn react link", async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const board = await screen.findByLabelText("game board");
+  expect(board).toBeInTheDocument();
 });
