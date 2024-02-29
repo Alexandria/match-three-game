313d2730e99f2fca4c64269a0177fc840beb247d
Lex <lexluka1031@gmail.com>
2024-02-28 21:42:30 -0600

    Update tests and fix infinite loop bug

--- src/components/utils/moveItemsDown.ts
+++ src/components/utils/moveItemsDown.ts
@@ -1,31 +1,39 @@
-import { forEach } from "lodash";
+import { forEach, every } from "lodash";
 import { Board } from "../types";
+import { generateRandomEmoji } from "./generateBoard";
 
 export const moveItemsDown = (board: Board): Board => {
-  const boardLength = board.length - 1;
   forEach(board, (row, rowIndex) => {
     forEach(row, (item, colIndex) => {
-      if (rowIndex >= boardLength) return;
+      if (rowIndex > 5) return;
       if (rowIndex === 0 && item.type === "") {
+        item.type = generateRandomEmoji();
         const itemBelow = board[rowIndex + 1][colIndex];
         const currentItem = board[rowIndex][colIndex];
         board[rowIndex][colIndex] = itemBelow;
         board[rowIndex + 1][colIndex] = currentItem;
+        board[rowIndex + 1][colIndex].animate = true;
       }
       if (item.type === "" && rowIndex > 0) {
         // this section may need to swap slower perhaps wrap in a function
+        if (rowIndex > 1) {
+          board[rowIndex - 2][colIndex].delay = 0.8;
+        }
         const itemAbove = board[rowIndex - 1][colIndex];
         const currentItem = board[rowIndex][colIndex];
         board[rowIndex][colIndex] = itemAbove;
         board[rowIndex - 1][colIndex] = currentItem;
+        board[rowIndex][colIndex].animate = true;
       }
     });
   });
 
-  if (
-    board[1].some((row, index) => row.type === "") &&
-    board[0].every((row, index) => row.type !== "")
-  ) {
+  const isEverySpotFilled = every(board, (row) => {
+    return every(row, (item) => item.type !== "");
+  });
+  console.log("isEverySpotFilled", isEverySpotFilled);
+
+  if (!isEverySpotFilled) {
     return moveItemsDown(board);
   }
   return board;
--- src/components/utils/tests/moveItemsDown.test.ts
+++ src/components/utils/tests/moveItemsDown.test.ts
@@ -1,11 +1,63 @@
-import { mockEmptyBoard } from "../../fixtures";
+import { mockBoard, mockEmptyBoard } from "../../fixtures";
+import { Board, BoardRow } from "../../types";
 import { moveItemsDown } from "../moveItemsDown";
 
+const updateRowInBoard = (
+  board: Board,
+  indexToReplace: number,
+  replacementRow: BoardRow
+) => {
+  const newBoard = [...board];
+  newBoard.splice(indexToReplace, 1, replacementRow);
+  return newBoard;
+};
 describe("moveItemsDown", () => {
-  it("will move item down if there is an empty space below", () => {
+  it("will move items down if there is an empty space below", () => {
     const board = moveItemsDown(mockEmptyBoard);
 
-    // expect(board).toBe();
+    expect(board[2]).toStrictEqual(
+      expect.arrayContaining([
+        { id: "11", type: "🍑" },
+        { id: "12", type: "🍑" },
+        { id: "8", animate: true, type: "🍌" },
+        { id: "9", animate: true, type: "🍌" },
+        { id: "10", animate: true, type: "🍑" },
+      ])
+    );
   });
-  it.todo("will not move item down if there is no empty space below it");
+  it("will not move item down if there is no empty space below it", () => {
+    const board = moveItemsDown(mockEmptyBoard);
+
+    expect(board[3]).toStrictEqual(
+      expect.arrayContaining([
+        { id: "16", type: "🥝" },
+        { id: "17", type: "🍌" },
+        { id: "18", type: "🥝" },
+        { id: "19", type: "🍒" },
+        { id: "20", type: "🥝" },
+      ])
+    );
+  });
+  it("will move item down if bottom row has an empty space", () => {
+    const rowToReplace = [
+      { id: "100", type: "🍑" },
+      { id: "101", type: "🥝" },
+      { id: "102", type: "" },
+      { id: "103", type: "" },
+      { id: "104", type: "" },
+    ];
+
+    const bottomRowHasEmptySpot = updateRowInBoard(mockBoard, 5, rowToReplace);
+    const board = moveItemsDown(bottomRowHasEmptySpot);
+
+    expect(board[5]).toStrictEqual(
+      expect.arrayContaining([
+        { id: "100", type: "🍑" },
+        { id: "101", type: "🥝" },
+        { id: "23", animate: true, type: "🍒" },
+        { id: "24", animate: true, type: "🍑" },
+        { id: "25", animate: true, type: "🥝" },
+      ])
+    );
+  });
 });
