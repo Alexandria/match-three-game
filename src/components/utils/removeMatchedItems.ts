7b16ec4ce8da20b660d0992a4e1029746db6f651
Lex <lexluka1031@gmail.com>
2024-04-16 13:19:41 -0500

    Clean up removeMatched items function

--- src/components/utils/removeMatchedItems.ts
+++ src/components/utils/removeMatchedItems.ts
@@ -2,49 +2,18 @@ import { forEach } from "lodash";
 import { Board, BoardItem } from "../types";
 import { checkForMatchesV2 } from "./checkForMatches";
 
-const removeById = (itemsToRemove: string[]) => (item: BoardItem) => {
-  if (itemsToRemove.includes(item.id)) {
-    item.type = "";
-  }
-};
-
-export const removeRowMatches = (
+export const removeMatchedItems = (
   board: Board,
   setScore: (newScore: number) => void
 ) => {
-  let matchRemoved = false;
-  const boardToCheck = board.slice(1);
-  forEach(boardToCheck, (row, rowIndex) => {
-    const rowMatchesFound = checkForMatchesV2(row);
-    if (rowMatchesFound) {
-      forEach(row, removeById(rowMatchesFound));
-      matchRemoved = true;
-    }
-  });
-
-  return matchRemoved;
-};
-
-export const removeColumnMatches = (
-  board: Board,
-  setScore: (newScore: number) => void
-) => {
-  let matchRemoved = false;
-  const boardToCheck = board.slice(1);
-  forEach(boardToCheck, (row, rowIndex) => {
+  forEach(board, (row, rowIndex) => {
     const column: BoardItem[] = [];
-
+    checkForMatchesV2(row);
     forEach(row, (value, colIndex) => {
-      const col = boardToCheck[colIndex][rowIndex];
+      if (!board[colIndex][rowIndex]) return;
+      const col = board[colIndex][rowIndex];
       column.push(col);
     });
-    const colMatchesFound = checkForMatchesV2(column);
-
-    if (colMatchesFound) {
-      forEach(column, removeById(colMatchesFound));
-      matchRemoved = true;
-    }
+    checkForMatchesV2(column);
   });
-
-  return matchRemoved;
 };
